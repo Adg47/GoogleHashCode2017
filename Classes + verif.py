78ac40559a93717549a13c5e03947e86d7bb472d
Tiphaine BESNARD <tiphaine.besnard@etu.univ-nantes.fr>
2017-11-15 17:47:13 +0100

    Téléverser un nouveau fichier

---
+++ Classes + verif.py
@@ -0,0 +1,152 @@
+####### Fonctions pour tester mon code ########
+def creation_cellules_Routeurs():
+    cellule1= Cellule([0,1], '#', False)
+    cellule2= Cellule([0,2], '-', False)
+    cellule3= Cellule([0,3], '#', False)
+    cellule4= Cellule([0,0], '-', False)
+
+    cellule5= Cellule([1,0], '.', False)
+    cellule6= Cellule([1,1], '#', False)
+    cellule7= Cellule([1,2], '.', False)
+    cellule8= Cellule([1,3], '-', False)
+    
+    cellule9= Cellule([2,0], '#', False)
+    cellule10= Cellule([2,1], '#', False)
+    cellule11= Cellule([2,2], '.', False)
+    cellule12= Cellule([2,3], '#', False)
+
+    routeur1 = Routeur(5,cellule1)
+    routeur2 = Routeur(5,cellule3)
+    routeur3 = Routeur(5,cellule6)
+  
+
+    liste_map = []
+    liste_map.append(cellule1)
+    liste_map.append(cellule2)
+    liste_map.append(cellule3)
+    liste_map.append(cellule4)
+    liste_map.append(cellule5)
+    liste_map.append(cellule6)
+    liste_map.append(cellule7)
+    liste_map.append(cellule8)
+    liste_map.append(cellule9)
+    liste_map.append(cellule10)
+    liste_map.append(cellule11)
+    liste_map.append(cellule12)
+
+    liste_routeurs=[]
+    liste_routeurs.append(routeur1)
+    liste_routeurs.append(routeur2)
+    liste_routeurs.append(routeur3)
+ 
+    backbone=[]
+    backbone.append(cellule1)
+    backbone.append(cellule4)
+    backbone.append(cellule2)
+    backbone.append(cellule5)
+    backbone.append(cellule6)
+    backbone.append(cellule7)
+    
+    return liste_map, liste_routeurs, backbone
+
+
+
+##### Vérification du type de case des routeurs et changement si besoin ####
+
+            
+def proposer_cellules_nonMur(routeur):
+    liste_coordVoisins = voisins(routeur.cellule.coord) #il récupère toutes les coord des voisins.
+    liste_voisines = [] #liste des cellules voisines à remplir
+    for i in range (len(liste_coordVoisins)): # parcours les coordonnées des voisins du routeur
+        if recherche_celluleNonMur_avecCoordonnees(liste_coordVoisins[i]): 
+            liste_voisines.append(recherche_celluleNonMur_avecCoordonnees(liste_coordVoisins[i]))
+            #ajoute la cellule aux voisines possibles si elle existe et n'est pas un mur
+
+    return liste_voisines # retourne la liste des voisines dispo
+
+
+def verif_MurCellule(cellule): #Permet de vérifier si une cellule est un mur ou non
+    if(cellule.type=='#'):
+        return True
+    else :
+        return False
+
+def verif_Cellule_libre(cellule): # Permet de savoir si une cellule est prise par un routeur ou non
+    for i in range (len(liste_routeurs)):
+        if(liste_routeurs[i].cellule == cellule): # si la cellule est dans la liste des routeurs alors non libre
+            return False #prise par routeur
+        else :
+            return True #libre
+    
+def recherche_celluleNonMur_avecCoordonnees(coordonnees): 
+# Permet de retrouver une cellule grâce aux coordonees et vérifie si ce n'est pas un mur
+# Retourne la cellule recherchée
+    i=0                      
+    cellule_parcourue = liste_map[0]
+    while( (cellule_parcourue.coord != coordonnees) and (i<len(liste_map))): #parcours toute la carte
+        cellule_parcourue = liste_map[i]
+        if(cellule_parcourue.coord==coordonnees): #si la cellule a les bonnes coordonnées
+            if (verif_MurCellule(cellule_parcourue)==False) and (verif_Cellule_libre(cellule_parcourue)==True): # si la cellule n'est pas de type mur
+               return cellule_parcourue #on retourne la cellule recherchée
+        else : #sinon on passe à la suivante
+            i+=1
+    
+def verif_tous_routeurs_pasDansMur(liste_routeurs_connectes): #Pour la vérification finale
+    for i in range (len(liste_routeurs_connectes)):
+        routeur = liste_routeurs_connectes[i]
+        if(routeur.cellule.type == '#'):
+            choix = proposer_cellules_nonMur(routeur)
+            routeur.cellule = choix[0] #prend une nouvelle cellule pour le routeur
+    print("nouvelle liste")
+    for i in range (len(liste_routeurs_connectes)):
+        print (liste_routeurs_connectes[i].cellule.coord)
+
+
+###### Classes #######
+
+class Cellule :
+    def __init__(self, coordonnees , Type, etat_couverture = False):
+        self.coord= coordonnees # entiers
+        self.type = Type # Mur, cible, vide (#, ., -)
+        self.etat_wifi = etat_couverture #True ou false
+
+class Routeur :
+    def __init__(self, rayon, cellule):
+        self.cellule = cellule
+        self.rayon = rayon
+        self.liste_cellules_couvertes = None #liste des cellules couvertes par le wifi
+
+    def ajout_cellules_dansListeCouverture(cellule):
+        self.liste_cellules_couvertes.append(cellule) #Ajoute une cellule à la liste
+
+
+##### Celles de Raph #####   
+
+def voisins(case):            #retourne la liste des coordonnées des cases voisines d'une CASE entrée
+    x=case[0]
+    y=case[1]
+    return([[x,y+1],[x,y-1],[x-1,y],[x-1,y+1],[x-1,y-1],[x+1,y+1],[x+1,y],[x+1,y-1]])
+
+###### Vérification du budget et calcul du score #####
+# récupéré les valeurs Pb, B et Pr dès le début
+
+def verif_budget():
+    if( ((len(backbone))*Pb + (len(liste_routeurs))*Pr) > B): #nbre cellules cablées et nbre routeurs
+        return "Hors budget !!!!!!"
+
+def score (liste_routeurs):
+    nbre_celluleWifi = 0
+    for i in range (len(liste_routeurs)):
+        nbre_celluleWifi += len(liste_routeurs[i].liste_cellules_couvertes)
+    return (1000*nbre_celluleWifi + (B-(N * Pb + M * Pr)))
+
+
+######################
+result=creation_cellules_Routeurs()
+liste_map = result[0]
+liste_routeurs = result[1]
+backbone = result[2]
+Pb = 12
+Pr = 1
+B = 55
+print(verif_budget())
