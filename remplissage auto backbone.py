2464afa50692b176568032dd1befe38de7f26f60
Raphael SANGLIER <raphael.sanglier@etu.univ-nantes.fr>
2017-11-21 17:51:24 +0100

    On rajoute un retour du nombre de case rajoutée à la backbone à chaque opération (pour la mise à jour du budget au fur et à mesure)

--- remplissage auto backbone.py
+++ remplissage auto backbone.py
@@ -26,14 +26,16 @@ def voisins(case):            #retourne la liste des coordonnées des cases vois
 routeur = [7,3]
 backbone=[[0,0],[1,0],[2,1],[3,1]]
 
-def relier(routeur,backbone):
+def relier(routeur,backbone):  #prend en compte seulement les coordonnées du routeur, et la liste des coordonnées des cases par lesquels passent la backbone 
     listdist=[] #liste des distances des points du backbone au routeur
     for a in backbone :
         listdist.append(dist(a,routeur))  #la listdist est remplie de distances des points de backbone au routeur dans le MEME ordre
     if mini(listdist)[0] < 1.4 : 
         print("le routeur est déjà relié à la backbone")
     else :
+        nb=1     #compteur du nombre de cases rajoutées au backbone (commence à 1 pour compter la case routeur)
         while mini(listdist)[0] >1.4:
+            nb+=1
             extremite=backbone[mini(listdist)[1]]       # l'extremité est la case du backbone la plus proche du routeur
             candidats=voisins(extremite)
             distcand=[]
@@ -42,4 +44,4 @@ def relier(routeur,backbone):
             backbone.append(candidats[mini(distcand)[1]])  #on rajoute la nouvelle case à backbone
             listdist.append(mini(distcand)[0])
         backbone.append(routeur)                           #on finit enfin par cabler le routeur
-    return(backbone)
+    return(backbone,nb)
