a4ae64eb4a70d6ee87d14620daaba28655694916
Alexis DE GUISTI <alexis.de-guisti@etu.univ-nantes.fr>
2017-11-29 18:59:10 +0100

    Update fct_fin.py

--- fct_fin.py
+++ fct_fin.py
@@ -1,16 +1,12 @@
-def fichier_de_fin(backbone,liste_routeur):
-	la_fin =open("bilan.text","a") # Ouvre/crée un fichier texte, utilise 'la_fin' afin de le traiter en python, écris en append, sans écraser mais en complétant :)
-	cellules_co_BN=fonction_backbone()
-	la_fin.write(cellules_co_BN,"cellules sont connectées au backbone \n")
-	for x in backbone:
-		la_fin.write("Cellule","[",x,"]","je sais pas quoi mettre, on trouvera plus tard \n")
-	la_fin.write(len(liste_routeur),"routeurs \n")
-	for y in liste_routeur:
-		la_fin.write("[",y,"]","est connecté au backbone et ce n'est pas un mur du coup, on met là voilà, donc ça nous met à l'aise =D \n")#Vous avez vraiment besoin de lire un commentaire après une ligne comme ça ?
-	la_fin.write(verif_budget(),"\n")
-	la_fin.write(score(liste_routeur))
+def fichier_de_fin(backbone,routeurs):
+	la_fin =open("bilan.text","a") # Ouvre/crée un fichier texte, utilise 'la_fin' afin de le traiter en python, écris en append, sans écraser mais en complétant 
+	la_fin.write(str(len(backbone))+"cellules sont connectées au backbone \n")#Nombre de cellules connectées au backbone
+	for x in range(len(backbone)):
+		la_fin.write("Cellule"+str(backbone[x][0])+ " "+str(backbone[x][1]+"voisine de la CIB \n") #Ecrit les cellules voisines du backbone
+	la_fin.write(len(liste_routeur),"routeurs \n")#Nombre de routeurs
+	for y in range(len(routeurs)):
+		la_fin.write(str(routeurs[y][0])+" "+str(routeurs[y][1] +"est connecté au backbone et ce n'est pas un mur du coup, on met là voilà, donc ça nous met à l'aise =D \n")#Ecrit les coordonnées de chaque routeur
+	la_fin.write(score(liste_routeur))#Ecrit score
 	la_fin.close()
-b = [(23,24),(458,89),(45,3216),(7854,481)]
-r = [(12,447),(4,652),(5698745)]
 	
-fichier_de_fin(b,r)
+fichier_de_fin(backbone, liste_routeurs) 
