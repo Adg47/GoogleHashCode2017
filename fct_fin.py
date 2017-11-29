4682cd02bd2ddd13852c4d18a16ca46f688b478e
Alexis DE GUISTI <alexis.de-guisti@etu.univ-nantes.fr>
2017-11-29 16:41:35 +0100

    Update fct_fin.py

--- fct_fin.py
+++ fct_fin.py
@@ -9,3 +9,8 @@ def fichier_de_fin(backbone,liste_routeur):
 		la_fin.write("[",y,"]","est connecté au backbone et ce n'est pas un mur du coup, on met là voilà, donc ça nous met à l'aise =D \n")#Vous avez vraiment besoin de lire un commentaire après une ligne comme ça ?
 	la_fin.write(verif_budget(),"\n")
 	la_fin.write(score(liste_routeur))
+	la_fin.close()
+b = [(23,24),(458,89),(45,3216),(7854,481)]
+r = [(12,447),(4,652),(5698745)]
+	
+fichier_de_fin(b,r)
